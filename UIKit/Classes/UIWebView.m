ca40f56d62fd48b430988f2fad4256d209e95901
Sérgio Silva <sergio.silva@bitrzr.com>
2012-09-17 20:23:49 +0100

    web view no longer impersonates an iPad but uses an application id as part of the user agent string

--- UIKit/Classes/UIWebView.m
+++ UIKit/Classes/UIWebView.m
@@ -54,7 +54,10 @@
         _webViewAdapter.scrollEnabled = NO;		// WebView does its own scrolling :/
         
         //impersonate an iPad.
-        _webView.customUserAgent = @"Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25";
+        //_webView.customUserAgent = @"Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25";
+        NSBundle *mainBundle = [NSBundle mainBundle];
+        
+        _webView.applicationNameForUserAgent =  [NSString stringWithFormat:@"%@/%@ (%@/%@)", [mainBundle objectForInfoDictionaryKey:@"CFBundleName"], [mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"], [mainBundle objectForInfoDictionaryKey:@"CFBundleIdentifier"], [mainBundle objectForInfoDictionaryKey:@"CFBundleVersion"]];
         [self addSubview:_webViewAdapter];
     }
     return self;
